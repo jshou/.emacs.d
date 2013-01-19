f0fc26c74ebd1c7a666606841f1beb2cfd7162e9
jshou <joshuahou@gmail.com>
2013-01-18 19:31:40 -0800

    initial commit

---
+++ init.el
@@ -0,0 +1,29 @@
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+(load-theme 'wombat)
+
+;; install multi-term
+(setq multi-term-program "/bin/zsh")
+(require 'multi-term)
+
+;; use utf-8 for everything
+;(setq locale-coding-system 'utf-8)
+;(set-terminal-coding-system 'utf-8)
+;(set-keyboard-coding-system 'utf-8)
+;(set-selection-coding-system 'utf-8)
+;(prefer-coding-system 'utf-8)
+
+;; Use Emacs terminfo, not system terminfo
+(setq system-uses-terminfo nil)
+
+;; set emacs font size
+(set-face-attribute 'default nil :height 100)
+
+;; show line numbers
+(global-linum-mode t)
+
+;; hide toolbars
+(tool-bar-mode -1)
+
+;; start ido
+(require 'ido)
+(ido-mode t)
