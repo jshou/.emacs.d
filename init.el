809081fd420e7eebc938623d536bc78ba6e9ea32
Joshua Hou <joshua@substantial.com>
2013-03-18 10:27:49 -0700

    set default local tracking branch name to not have remote in the beginning, change how magit windows open

--- init.el
+++ init.el
@@ -62,3 +62,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(term-color-blue ((t (:background "deep sky blue" :foreground "deep sky blue")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only))
+ '(magit-status-buffer-switch-function (quote switch-to-buffer)))
