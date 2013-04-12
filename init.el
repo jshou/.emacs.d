3d4d448f1b132256bd32644b46e1a62caffb856c
Joshua Hou <joshua@substantial.com>
2013-04-12 12:56:45 -0700

    make hamlbars use haml mode
    Conflicts:
    	init.el

--- init.el
+++ init.el
@@ -40,6 +40,8 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x C-b") 'buffer-menu)
 
+(add-to-list 'auto-mode-alist '(".hamlbars'" . haml-mode))
+
 ;; set ruby indentation to something sensible
 (setq ruby-deep-indent-paren nil)
 
